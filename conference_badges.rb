5b46f1e2bf00f8f2d9a51ab722ec9cfe1f38471f
Vincent Langat <kimutailangat033@gmail.com>
2022-08-24 14:27:12 +0300

    commit tree 1e106813912d356d04b616c5ed893f59938fbe8b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name = "Arel")
+    "Hello, my name is #{name}."
+end
+
+  def assign_rooms(names_array)
+    room_number = 0
+    names_array.map do |name|
+        "Hello, #{name}! You'll be assigned to room #{room_number += 1}!"
+    end
+end
+def batch_badge_creator (badges)
+    badges.map do |b|
+      "Hello, my name is #{b}."
+    end
+end
+def printer(names_array)
+    batch_badge_creator(names_array).each do |badge|
+        puts badge
+    end
+    assign_rooms(names_array).each do |room_info|
+        puts room_info
+    end
+end
+
+
+
